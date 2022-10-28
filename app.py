8152dee3d6a18718fc89f85bdd8e85b179cee8fa
Yousef <yousefbelal265@gmail.com>
2022-10-28 23:31:37 +0200

    init

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def add_to_db(video_id):
 	if result:
 		abort(409, message="Video id taken...")
 
-	video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'],date=dt_object,trending=args['trending'])
+	video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'],date=dt_object)
 	db.session.add(video)
 	db.session.commit()
 	return video, 201
